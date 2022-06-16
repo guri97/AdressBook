79534cb70343ad67ee8f8094e397a80bc075e0e8
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-16 18:43:36 +0530

    [gurpreet] UC1_CreateContact

---
+++ AdressBook/AdressBook/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+    }
+}
+
---
+++ AdressBook/AdressBook/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AddressBook
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook");
+
+            AdressBook address = new AdressBook();
+
+            string command = "";
+
+            while (command != "exit")
+            {
+
+                Console.WriteLine("Please enter a command add,edit,remove,display,multiplperson: ");
+                command = Console.ReadLine().ToLower();
+
+                switch (command)
+                {
+                    case "add":
+
+
+                        address.AddPerson();
+                        break;
+
+                    case "edit":
+                        address.EditContact();
+                        break;
+                    case "remove":
+                        address.RemoveContact();
+                        break;
+                    case "display":
+                        address.Display();
+                        break;
+                }
+            }
+
+        }
+    }
+}
+
