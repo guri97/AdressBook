67c8594b7d3075327f42be2af3fe7651cd98581d
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-19 11:32:03 +0530

    [gurpreet] UC6_UniqueNameForAdressBook

---
+++ UC6/AdressBookSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressbook1
+{
+    internal class AdressBookSystem
+    {
+        public Dictionary<string, AdressBook> adressBooks = new Dictionary<string, AdressBook>();
+        public void AddAddressBook()
+        {
+            AdressBook adressBook = new AdressBook();
+            adressBook.AddMultipleContact();
+            Console.WriteLine("enter the addressbook name");
+            string addressName = Convert.ToString(Console.ReadLine());
+            adressBooks.Add(addressName.ToLower(), adressBook);
+
+        }
+    }
+}
---
+++ UC6/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace AdressBook1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to AdressBook");
+            AdressBookSystem adressBookSystem = new AdressBookSystem();
+            string command1 = "";
+
+            while (command1 != "exit")
+            {
+                Console.WriteLine("enter the option: 1.Add AdressBook 2.Edit AddressBook 3.Remove AddressBook");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        adressBookSystem.AddAddressBook();
+                        break;
+                    case 2:
+                        Console.WriteLine("enter the addressbook name you want to edit");
+                        string addressBookName = Console.ReadLine();
+
+
+                        if (adressBookSystem.adressBooks.ContainsKey(addressBookName.ToLower()))
+                        {
+
+
+                            string command = "";
+
+                            while (command != "exit")
+                            {
+
+                                Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+                                command = Console.ReadLine().ToLower();
+
+
+                                switch (command)
+                                {
+                                    case "add":
+
+
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].AddPerson();
+                                        break;
+                                    case "edit":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].EditContact();
+                                        break;
+                                    case "remove":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].RemoveContact();
+                                        break;
+                                    case "display":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].Display();
+                                        break;
+                                    case "multipleperson":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].AddMultipleContact();
+                                        break;
+                                    default:
+                                        command = "exit";
+                                        break;
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        command1 = "exit";
+                        break;
+
+
+
+
+
+                }
+
+            }
+        }
+    }
+}
